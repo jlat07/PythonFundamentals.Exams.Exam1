baa7a56db27289b98beba98c56f0fbef4368d91c
JLAT <jlat0128@gmail.com>
2020-03-18 11:56:16 -0400

    update generate list with strategy method

--- exam1/loopty_loop.py
+++ exam1/loopty_loop.py
@@ -23,6 +23,6 @@ def generate_list_with_strategy(start: int, stop: int, step: int, strategy: Call
     :param strategy: A function to manipulate each digit .
     :return: A list of integers.
     """
-    list(range(start, stop, step, callable(strategy)))
-    
+    list = list(range(start, stop, step)
+    list =[(strategy) for x in list]
     return list
